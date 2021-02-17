321e48845c91c838bade42322424a4c4af87ba88
DESKTOP-VA4KGMI\Huỳnh Thanh Lâm <htlam164@gmail.com>
2021-02-17 21:18:01 +0700

    update

--- server_basic.js
+++ server_basic.js
@@ -2,7 +2,7 @@ var http = require("http");
 
 var server = http.createServer(function(reg, res){
     res.writeHead(200, {"Content-Type": "text/plain"});
-    res.write("Hello, This is the first webserver with NodeJS");
+    res.write("Hello, Thao Nhi Dang Iu");
     res.end();
 });
 
