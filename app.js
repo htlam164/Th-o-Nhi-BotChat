8fa6c68517ae3898f1da0a6a38bdef7d4a49c10b
DESKTOP-VA4KGMI\Huỳnh Thanh Lâm <htlam164@gmail.com>
2021-02-22 15:36:31 +0700

    abc

--- app.js
+++ app.js
@@ -6,17 +6,17 @@ var app = express();
 app.set("views", __dirname + "/apps/views");
 app.set("view engine", "ejs");
 
+const PORT = process.env.PORT || 3000;
 // Static folder
 
 var controllers = require(__dirname + "/apps/controllers");
 
 app.use(controllers);
 
-var server = app.listen(process.env.PORT, function() {
-    console.log("Server is running on port", port);
+var server = app.listen(PORT, function() {
+    console.log("Server is running on port", PORT);
 });
 // // process.env.PORT
 ///asd
-
 var io = socketio(server);
 var socketcontrol = require("./apps/common/socketcontrol")(io);
