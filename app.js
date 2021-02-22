77c417715d5fde0bb84cfdbbed273c77f0fa6913
DESKTOP-VA4KGMI\Huỳnh Thanh Lâm <htlam164@gmail.com>
2021-02-22 13:58:56 +0700

    chat update

---
+++ app.js
@@ -0,0 +1,33 @@
+var express = require("express");
+var config = require("config");
+var bodyParser = require("body-parser");
+
+var socketio = require("socket.io");
+
+var app = express();
+//body parser
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+app.set("views", __dirname + "/apps/views");
+app.set("view engine", "ejs");
+
+// Static folder
+app.use("/static", express.static(__dirname + "/public"));
+
+var controllers = require(__dirname + "/apps/controllers");
+
+app.use(controllers);
+
+var host = config.get("server.host");
+var port = config.get("server.port");
+var server = app.listen(process.env.PORT || port, host, function() {
+    console.log("Server is running on port", port);
+});
+// //
+
+var io = socketio(server);
+var socketcontrol = require("./apps/common/socketcontrol")(io);
